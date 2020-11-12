3e8e7e456052ce67863044751352c17b958f5136
Ricimon <ricimon25@gmail.com>
2020-11-12 02:46:01 -0800

    simplified dev server run/stop process

--- run-docker-dev.sh
+++ run-docker-dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -p ricimon_com-dev -f docker-compose.dev.yml up -d --build && docker exec -it ricimon.com-dev sh -c "yarn start"
+docker-compose -p ricimon_com-dev -f docker-compose.dev.yml up --build
--- stop-docker-dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker-compose -p ricimon_com-dev -f docker-compose.dev.yml down
