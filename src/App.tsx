1cf25c4d9964bdb58230ede1877b3aefb64be391
Ricimon <ricimon25@gmail.com>
2020-11-14 20:58:40 -0800

    Add react-helmet, set title and description tags of website. run-docker-dev now runs docker-compose stop on ctrl+c signal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import './App.css';
 import githubIcon from './res/github-icon.svg';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Home - Ricimon</title>
+        <meta name="description" content="Ricimon's personal website" />
+      </Helmet>
       <div className="App-main">
         <h1><span>
           Hi! I'm <span className="Decorated-below" data-content="(ree-see-mon)">Ricimon</span>
